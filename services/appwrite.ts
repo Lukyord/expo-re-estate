60205793a062a56827925fa733da39c0d92a44ab
plaimanas-client <plaimanas-client@gmail.com>
2025-03-06 18:04:11 +0700

    chore: add db info to appwrite config

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,9 +1,14 @@
 import { Platform } from "react-native";
-import { Account, Avatars, Client } from "react-native-appwrite";
+import { Account, Avatars, Client, Databases } from "react-native-appwrite";
 
 export const config = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOTIN || "",
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID || "",
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID || "",
+    galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID || "",
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID || "",
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID || "",
+    propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID || "",
 };
 
 export const client = new Client();
@@ -21,3 +26,4 @@ switch (Platform.OS) {
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+export const databases = new Databases(client);
