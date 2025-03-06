b9cbacb0ebcaa1f93ac7148c2859c0bfdc4f0322
plaimanas-client <plaimanas-client@gmail.com>
2025-03-06 14:36:01 +0700

    feat: add sign-in page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,5 +26,11 @@ export default function RootLayout() {
         return null;
     }
 
-    return <Stack />;
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: false,
+            }}
+        />
+    );
 }
