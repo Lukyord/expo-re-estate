a2475284b7bf49e7541130dd4b5845c530253b55
plaimanas-client <plaimanas-client@gmail.com>
2025-03-06 16:27:49 +0700

    feat: add tabs

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,10 +11,6 @@ export default function Index() {
             }}
         >
             <Text className="font-bold text-lg my-10 font-rubik">Welcome to Restate</Text>
-            <Link href="/sign-in">Sign In</Link>
-            <Link href="/explore">Explore</Link>
-            <Link href="/profile">Profile</Link>
-            <Link href="/properties/1">Property 1</Link>
         </View>
     );
 }
